ab4ca8b27289dd5a2b7a95e5cf27864f21ade61f
Chris Johns <tyrantwave@gmail.com>
2019-02-04 15:03:35 +0900

    Fractal results now return a struct with both the escape value, and the finalised z value.

--- src/fractal.rs
+++ src/fractal.rs
@@ -2,21 +2,45 @@ use num::complex::Complex;
 
 use std::str::FromStr;
 
+/// Result given from a fractal calculation.
+/// `escape`: Iterations needed to escape the function, 0 if it did not escape.
+/// `value`: Final z value, whether it escaped or not.
+#[derive(Clone, Debug)]
+pub struct FractalResult {
+    pub escape: u32,
+    pub value: Complex<f64>,
+}
+
+impl FractalResult {
+    pub fn zero() -> Self {
+        FractalResult {
+            escape: 0,
+            value: Complex { re: 0.0, im: 0.0 },
+        }
+    }
+}
+
 /// Try to determine if `c` is in the Mandelbrot set, using
 /// at most `limit` iterations.
 ///
 /// If `c` is not a member, return `Some(i)`, where `i` is the number of iterations
 /// taken to escape. If `c` appears to be a member, return `None`.
-fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+fn escape_time(c: Complex<f64>, limit: u32) -> FractalResult {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
         z = z * z + c;
         if z.norm_sqr() > 4.0 {
-            return Some(i);
+            return FractalResult {
+                escape: limit - i,
+                value: z,
+            };
         }
     }
 
-    None
+    FractalResult {
+        escape: 0,
+        value: z,
+    }
 }
 
 /// Parse the string `s` as a coordinate pair, like `"400x600"` or `"1.0,1.5"`.
@@ -74,7 +98,7 @@ pub fn pixel_to_point(
 /// which holds one greyscale pixel per byte. The `upper_left` and `lower_right`
 /// arguments specify points on the complex plane corresponding to the given corners of the pixel buffer.
 pub fn render_mandelbrot(
-    pixels: &mut [u8],
+    pixels: &mut [FractalResult],
     bounds: (usize, usize),
     upper_left: Complex<f64>,
     lower_right: Complex<f64>,
@@ -84,10 +108,7 @@ pub fn render_mandelbrot(
     for row in 0..bounds.1 {
         for column in 0..bounds.0 {
             let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
-            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
-                None => 0,
-                Some(count) => 255 - count as u8,
-            };
+            pixels[row * bounds.0 + column] = escape_time(point, 255);
         }
     }
 }
